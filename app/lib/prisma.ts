611886242e39ee2055339bd3f9ae3b9d509c75da
Dominik Gadzina <epros98@gmail.com>
2025-02-13 00:47:25 +0100

    changed back prisma

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,16 +1,5 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // Prevent multiple instances of PrismaClient in development
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
